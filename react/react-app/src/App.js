61c2c311da0bc862775c11b1f02200398190f157
Tarcisio Coutinho <tcs5cin@gmail.com>
2019-05-04 22:21:51 -0300

    radium lib

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -49,7 +50,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover' : {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -69,6 +74,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'lightcoral',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -98,4 +107,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
